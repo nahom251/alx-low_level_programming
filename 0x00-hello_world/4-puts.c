355023f037d65c8755a65dbd06876c3dc5bcbc98
nahom251 <nahomdaniel4@gmail.com>
2023-02-16 13:07:13 -0800

    hhelp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/*description for main*/
 int main(void)
+	/*description for main*/
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
