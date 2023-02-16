5d8b5f0aa9de3184908e655ba1807f69e9f008fb
nahom251 <nahomdaniel4@gmail.com>
2023-02-16 14:35:11 -0800

    waguwan

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints different siye types
+ * Return: 0.
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of an long long int: %zu byte(s)\n", sizeof(long long int));
+return (0);
+}
