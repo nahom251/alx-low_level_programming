95f7221d3ba0d326070488e749911bd39f9b4b8f
nahom251 <nahomdaniel4@gmail.com>
2023-02-17 07:13:23 +0000

    simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - progtam that prins without print functions
  * Return: 1
  */
-int main(1)
+int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (1);
+return (0);
 }
