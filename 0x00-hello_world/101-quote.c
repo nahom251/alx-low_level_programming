01fd5147a5cb456d7df63b0e9112e39024ab7cc2
nahom251 <nahomdaniel4@gmail.com>
2023-02-17 07:37:01 +0000

    smple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include <unistd.h>
-/*
+/**
  * main - progtam that prins without print functions
  * Return: Always 1
  */
-intmain(void)
+int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
