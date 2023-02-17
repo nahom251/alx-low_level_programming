871a3274300c0276475de78503334b79bfc27a7b
nahom251 <nahomdaniel4@gmail.com>
2023-02-17 07:11:31 +0000

    simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+ * main - progtam that prins without print functions
+ * Return: 1
+ */
+int main(1)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
