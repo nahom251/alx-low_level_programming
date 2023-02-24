df755c3229258ad72885bd1c65e13155ff218fef
nahom251 <nahomdaniel4@gmail.com>
2023-02-24 19:14:42 +0000

    multply integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: input integer
+ * @b: input intege
+ * Return: their multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
