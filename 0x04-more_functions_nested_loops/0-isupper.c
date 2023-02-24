6cecf5618fd8193921bc2acf4e6ad9825b9320db
nahom251 <nahomdaniel4@gmail.com>
2023-02-24 18:41:06 +0000

    worked

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - prints uppercase letters
+ * @c: char to check
+ * Return: 1 and 0 depending on condition
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+oxo4-more_functions_nested_loops
