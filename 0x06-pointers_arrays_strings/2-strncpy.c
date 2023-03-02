d0c711ff86b79444112884f59614232e7cfa7654
nahom251 <nahomdaniel4@gmail.com>
2023-03-02 18:46:08 +0000

    sd

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * *_strncpy - copies string
+ * _strncpy - copies string
  * @dest: input
  * @src: input
  * @n: input
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
