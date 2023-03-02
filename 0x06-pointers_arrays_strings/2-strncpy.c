3ca76790e2816df7fe7717bf4519c77c7e671be9
nahom251 <nahomdaniel4@gmail.com>
2023-03-02 18:39:50 +0000

    sd

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncpy - copies string
+ * @dest: input
+ * @src: input
+ * @n: input
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
+}
+
