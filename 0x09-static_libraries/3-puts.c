f631506d08b4b0dd8636a246b56c9444155e0052
nahom251 <nahomdaniel4@gmail.com>
2023-03-10 19:41:47 +0000

    sdk

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
-/**
- * _puts - prints string
- * @str: string
- */
 void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
