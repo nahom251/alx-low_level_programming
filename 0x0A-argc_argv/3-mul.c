540d8405ccd1e3d7848d48d37aafe9cd9b03bb08
nahom251 <nahomdaniel4@gmail.com>
2023-03-12 17:47:33 +0000

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc < 2)
-{
-printf("Error\n");
-}
-else
+if (argc > 2)
 {
 int mul;
 
 mul = atoi(argv[1])  * atoi(argv[2]);
 printf("%d\n", mul);
-}
-
 return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
 }
