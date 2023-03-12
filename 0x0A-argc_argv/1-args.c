303b03e6414ccc90134ce0cda738d5955fa3b5d3
nahom251 <nahomdaniel4@gmail.com>
2023-03-12 16:54:12 +0000

    sd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- prints number of arguments
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
