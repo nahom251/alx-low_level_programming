66aee96a1ce08027c3d15b43809253b4d4bb49b5
nahom251 <nahomdaniel4@gmail.com>
2023-04-04 18:42:16 +0000

    sd

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns elements in a linked list
+ * @h: pointer to the list_t
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
